467568adb0fd6827edba97d6aff8db341555b6fa
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2017-12-22 17:50:12 +0545

    chore: Changed enry point

--- src/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-const App = () => {
-  return (
-    <h1>This is App!! versio</h1>
-  )
-}
-render(<App />, document.getElementById('app'));
