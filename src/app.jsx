ef3e73d7c0e40d3350e7d82f1fd47bae52ff65e6
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2017-12-22 11:42:03 +0545

    chore: Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>This is App!! versio</h1>
+  )
+}
+render(<App />, document.getElementById('app'));
