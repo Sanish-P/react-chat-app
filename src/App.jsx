4d59467e20b7a9c77fcb9f736dc481102d9eff33
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2018-01-08 22:01:28 +0545

    feat: Added chat route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './scenes/Login/Login.jsx';
+import LetsChat from './scenes/Lets-chat/lets-chat.jsx';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={Login} />
+      <Route exact path="/lets-chat" component={LetsChat} />
       <Route component={() => (<h1>404</h1>)} />
     </Switch>
   </BrowserRouter>
