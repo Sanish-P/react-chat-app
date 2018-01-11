2b1ce0b63591cea4eb274da59e4a3da04c86f4fa
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2018-01-11 17:37:40 +0545

    feat: Added chat home route

---
+++ src/scenes/Lets-chat/lets-chat.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'src/utils/axios';
+
+const LetsChat = props => {
+  const testToken = () => {
+    const accessToken = window.sessionStorage.getItem('access_token');
+    axios.get('/super-secret-resource',{
+      headers: { 'Authorization': `Bearer ${accessToken}` }
+    })
+    .then(({ data }) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+  testToken();
+  return (
+    <div>
+      <h1>This is where chat happens...</h1>
+    </div>
+  );
+};
+
+export default LetsChat;
