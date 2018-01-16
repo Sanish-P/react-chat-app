4a6689a7215baea3a33f778a8fa9d82e806691f8
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2018-01-16 21:59:25 +0545

    feat: Set logic for refresh token

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,24 +3,53 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://localhost:3000',
   timeout: 1000
-})
+});
+let savedConfig;
 
-const accessToken = window.sessionStorage.getItem('access_token');
+function _useRefreshToken() {
+  let refreshToken = window.sessionStorage.getItem('refresh_token');
+  let userId = window.sessionStorage.getItem('user_id');
+  let reqData = {
+    'grant_type': 'refresh_token',
+    'refresh_token': refreshToken,
+    'user_id': userId
+  }
+  instance.post('/auth/token', reqData).then(function ({data}) {
+    console.log('Access token refreshed using refresh token');
+    window.sessionStorage.setItem('access_token', data.access_token);
+    window.sessionStorage.setItem('refresh_token', data.refresh_token);
+    _continuePrevReq();
+  })
+}
+function _continuePrevReq() {
+  console.log('I am continuing prev request ...');
+}
 
-instance.interceptors.request.use(function (config) {
-  (typeof accessToken === 'string') && (config.headers['Authorization'] = `Bearer ${accessToken}`);
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-})
+instance.interceptors.request.use(
+  function(config) {
+    if (config.url !== '/auth/token') {
+      savedConfig = config;
+      let accessToken = window.sessionStorage.getItem('access_token');
+      typeof accessToken === 'string' &&
+      (config.headers['Authorization'] = `Bearer ${accessToken}`);
+    }
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
 
 instance.interceptors.response.use(
-  function (response) {
+  function(response) {
     return response;
   },
-  function (err) {
+  function(err) {
+    if (err.response.status === 401) {
+      _useRefreshToken();
+    }
     return Promise.reject(err);
-  })
-
+  }
+);
 
 export default instance;
