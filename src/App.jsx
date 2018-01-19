1657528d147fb2ecaea1c6c0af8f974560576494
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2018-01-19 17:35:56 +0545

    refactor: Shifted history to util

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import Login from './scenes/Login/Login.jsx';
 import LetsChat from './scenes/Lets-chat/lets-chat.jsx';
+import history from 'src/utils/history.js';
+import PrivateRoute from 'src/components/PrivateRoute/PrivateRoute.jsx';
 
-const App = () => (
-  <BrowserRouter>
+const App = (props) => (
+  <Router history={history}>
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route exact path="/lets-chat" component={LetsChat} />
+      <Route exact path="/lets-chat" component={PrivateRoute(LetsChat)} />
       <Route component={() => <h1>404</h1>} />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
