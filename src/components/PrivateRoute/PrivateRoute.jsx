97e5cc49136a9edd4efb81446a5467fbb6bacbe3
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2018-01-19 17:41:52 +0545

    refactor: Added private route

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+
+function PrivateRoute(WrappedComponent) {
+  return (props) => {
+    const checkAccessToken = () => {
+      return (typeof sessionStorage.getItem('access_token') === 'string')
+    }
+    if (checkAccessToken()) {
+      return (
+        <WrappedComponent {...props} />
+      )
+    } else {
+      return (
+        <Redirect to="/" />
+      )
+    }
+  }
+}
+
+export default PrivateRoute;
